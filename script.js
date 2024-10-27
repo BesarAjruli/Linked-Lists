a7c0c07e27fe0188c2d98270c7d0e4b09cd7fc23
BesarAjruli <aquila1912@gmail.com>
2024-10-27 21:34:35 +0100

    Finished project

---
+++ script.js
@@ -0,0 +1,123 @@
+class Node {
+  constructor() {
+    this.value = null;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(value) {
+    if (!this.head) {
+      this.head = new Node();
+      this.head.value = value;
+    } else {
+      let newNode = new Node();
+      newNode.value = value;
+      let end = this.head;
+      while (end.nextNode) {
+        end = end.nextNode;
+      }
+      end.nextNode = newNode;
+    }
+  }
+
+  prepend(value) {
+    this.head = new Node();
+    this.head.value = value;
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node.nextNode;
+    }
+    return count;
+  }
+
+  head() {
+    if (this.head) {
+      return this.head.value;
+    }
+  }
+
+  tails() {
+    if (this.head) {
+      let tail = this.head;
+      while (tail.nextNode) {
+        tail = tail.nextNode;
+      }
+      return tail;
+    }
+  }
+
+  at(index) {
+    let nodePosition = 0;
+    let node = this.head;
+    while (node) {
+      if (index === nodePosition) {
+        console.log(`Element ${node.value} at index ${index}`);
+        return;
+      } else {
+        node = node.nextNode;
+        nodePosition++;
+      }
+    }
+  }
+
+  pop() {
+    if (!this.head) {
+      return `No elements to pop!!!`;
+    } else if (this.head && !this.head.nextNode) {
+      this.head = null;
+    } else {
+      let pop = this.head;
+      while (pop) {
+        if (pop.nextNode && pop.nextNode.nextNode) {
+          pop = pop.nextNode;
+        } else break;
+      }
+      pop.nextNode = null;
+    }
+  }
+
+  contains(value) {
+    let node = this.head;
+    while (node) {
+      if (node.value === value) {
+        return true
+      }
+      node = node.nextNode
+    }
+    return false
+  }
+
+  find(value){
+    let index = 0
+    let node = this.head;
+    while (node) {
+      if (node.value === value) {
+        return index
+      }
+      node = node.nextNode
+      index++
+    }
+    return null
+  }
+
+  toString() {
+    let listPrint = ''
+    let list = this.head
+    while (list){
+        listPrint += `( ${list.value} ) -> `
+        list = list.nextNode
+    }
+    console.log(listPrint + 'null')
+  }
+}
+
+export {LinkedList}
